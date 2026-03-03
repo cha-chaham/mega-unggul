6aabdbc4928dd1ca964a199cd7b4f0a887fddda5
cha_chaham <charliech1056@gmail.com>
2026-03-03 19:24:44 +0800

    feat: add speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "@/routes";
 import "@/styles/index.css";
 import { ThemeProvider } from "./utils/contexts/theme";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <SpeedInsights>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </SpeedInsights>
   </React.StrictMode>
 );

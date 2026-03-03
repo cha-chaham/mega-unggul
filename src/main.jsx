cdddb8a331c27327ce9e4f895a6a14d2341c15c3
cha_chaham <charliech1056@gmail.com>
2026-03-03 19:30:12 +0800

    fix: speedinsight vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "@/routes";
 import "@/styles/index.css";
 import { ThemeProvider } from "./utils/contexts/theme";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SpeedInsights>
       <ThemeProvider>
         <App />
+        <SpeedInsights/>
       </ThemeProvider>
-    </SpeedInsights>
   </React.StrictMode>
 );
